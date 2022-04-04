71f153c3ac00259a773d9268855bb065a7075229
Maciej Barć <xgqt@gentoo.org>
2022-04-04 23:44:52 +0200

    src/el-fetch/el-fetch.el: adjust spacing
    Signed-off-by: Maciej Barć <xgqt@gentoo.org>

--- src/el-fetch/el-fetch.el
+++ src/el-fetch/el-fetch.el
@@ -39,7 +39,6 @@
 
 ;; RKTFetch: https://github.com/mythical-linux/rktfetch
 
-
 ;; Though, this is not a re-implementation;
 ;; this program is meant to extend "fetch" to this new domain, i.e. Emacs Lisp.
 ;; El-Fetch does not implement some of Neofetch's features users of the program
@@ -47,10 +46,8 @@
 ;; El-Fetch adds some Emacs-specific information gathering,
 ;; e.g.: Emacs version/packages, used theme, time spent in the editor.
 
-
 ;; WARNING: El-Fetch is primarily developed on GNU/Linux,
 ;; Windows support is experimental, macOS support is totally untested.
-
 
 ;; To run El-Fetch add it to your load-path,
 ;; execute M-x load-library el-fetch and then M-x el-fetch
