237d46cbae5a43d9210589f17204e51fa7d0e974
Maciej Barć <xgqt@gentoo.org>
2023-06-24 01:16:00 +0200

    add .dir-locals.el
    Signed-off-by: Maciej Barć <xgqt@gentoo.org>

---
+++ .dir-locals.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t -*-
+
+;; Directory Local Variables
+;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((projectile-project-type . make)))
+ (find-file . ((indent-tabs-mode . nil)
+               (require-final-newline . t)
+               (show-trailing-whitespace . t)))
+ (makefile-mode . ((indent-tabs-mode . t)))
+ (yaml-mode . ((tab-width . 2))))
